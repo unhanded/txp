b9fa79901e0aea126ba6a27d0b4341b516f1309a
Lukas Sondell <sondelln@gmail.com>
2025-06-12 11:06:09 +0200

    Added regular err-returning delete

--- internal/fs/del.go
+++ internal/fs/del.go
@@ -6,6 +6,10 @@ import (
 	"github.com/charmbracelet/log"
 )
 
+func FileDelete(fp string) error {
+	return os.Remove(fp)
+}
+
 func UnbotheredDelete(fp string) {
 	if err := os.RemoveAll(fp); err != nil {
 		log.Warn("Delete failed", "err", err)
